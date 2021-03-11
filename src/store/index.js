ca226a9a80dd62eacbb658a85bab2f859ff1bc2b
Meduoye Oluwafemi <39339729+femoxmed@users.noreply.github.com>
2021-03-11 23:18:03 +0100

    setting up heroku deployment

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import AuthReducer from './reducers/authReducer';
-
+//heroku deploy
 // import apodReducer from './reducers/apod';
 
 // compose enhancers
